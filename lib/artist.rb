05425f0d8e87c94e7bbc093da661b00df2df97e2
Katie Ashlin <akka726@outlook.com>
2018-11-25 22:26:06 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist 
 
-  attr_accessor :name, :songs
+  attr_accessor :name
   
   def initialize(name)
     @name = name
@@ -8,11 +8,11 @@ class Artist
 
   end
   
-    def add_song(song)
-      @songs << song
-      song.artist = self
-      
-    end
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
   
+  def song
 
 end
